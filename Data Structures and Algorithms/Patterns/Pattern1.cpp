d5bc5ab4e1dc9d55f48a7f289e328e2c47f0a103
Anisha Shende <anishashende963@gmail.com>
2023-09-26 23:05:23 +0530

    second commit

--- Data Structures and Algorithms/Patterns/Pattern1.cpp
+++ Data Structures and Algorithms/Patterns/Pattern1.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 using namespace std;
 
-    int main(){
-        for(int i = 0; i < 4; i ++){
-            for (int j = 0; j < 4; j++)
+    void Pattern1(int num){
+        // * * * *
+        // * * * *
+        // * * * *
+        // * * * * 
+        for(int i = 0; i < num; i ++){
+            for (int j = 0; j < num; j++)
             {
                 cout<<"* ";
             }
             cout<<endl;
         }
+    }
+
+    int main(){
+        int num;
+        cout << "Enter a number : ";
+        cin >> num;
+        Pattern1(num);
         return 0;
     }
